128e328051d485c5cf248d8357a8e989a8a7de19
Vinni Hoke <vinni.hoke@gmail.com>
2019-08-05 19:50:47 -0700

    Stretch Completed

--- starwars/src/components/CharacterCard/CharacterCard.js
+++ starwars/src/components/CharacterCard/CharacterCard.js
@@ -8,6 +8,7 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+
 // import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Fab from '@material-ui/core/Fab';
@@ -35,7 +36,7 @@ export default function CharacterCard({ character }) {
 
   return (
     <Card className={`${classes.card} CardContainer`}>
-      <CardActionArea>
+      <CardActionArea onClick={() => alert('This would give you a full snapshot of the characters information in their own profile view.')}>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {character.name}
@@ -61,10 +62,10 @@ export default function CharacterCard({ character }) {
         </CardContent>
       </CardActionArea>
       <CardActions className="Actions">
-        <Button size="small" variant="outlined" color="primary">
+        <Button size="small" variant="outlined" color="primary" onClick={() => alert('This would give you the full filmography of the character')}>
           Filmography
         </Button>
-        <Fab size="small" className={classes.button} color="secondary">
+        <Fab size="small" className={classes.button} color="secondary" onClick={() => alert('This would eventually save your characters in a new array.')}>
           <Icon>favorite</Icon>
         </Fab>
       </CardActions>
