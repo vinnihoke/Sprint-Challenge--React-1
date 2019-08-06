0f9edfdfcc8adbf7b0b9ebb555fa7578899d2707
Vinni Hoke <vinni.hoke@gmail.com>
2019-08-05 20:33:36 -0700

    Netlify Fix

---
+++ src/App.js
@@ -0,0 +1,66 @@
+// React Imports
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Material Icons
+import Icon from '@material-ui/core/Icon'
+
+// Local Imports
+import './scss/App.scss';
+import CharacterCard from './components/CharacterCard/CharacterCard';
+
+
+
+
+const App = () => {
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
+
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [header, setHeader] = useState('React Wars');
+
+  useEffect(() => {
+    setHeader('Content Loading...');
+    axios
+    .get(`https://swapi.co/api/people/?page=${page}`)
+    .then(results => {
+      setCharacters(results.data.results);
+      setHeader('React Wars');
+    })
+    .catch(err => {
+      console.log(err);
+      setHeader("Content Load Error");
+      alert('There was an error loading the data. Please try again later.')
+    })
+  }, [page])
+
+  const pageUp = () => {
+    return (page < 9 ? setPage(page + 1) : null);
+  }
+  const pageDown = () => {
+    return (page > 1 ? setPage(page - 1) : null);
+  }
+
+  return (
+    <div className="App">
+      <h1 className="Header">{header}</h1>
+
+      <button className="ButtonUp" onClick={pageUp}><Icon>keyboard_arrow_right</Icon></button>
+      <button className="ButtonDown" onClick={pageDown}><Icon>keyboard_arrow_left</Icon></button>
+
+      <div className="CardHolder">
+        {characters.map((character, index) => {
+          return <CharacterCard key={index} character={character}/>
+        })}
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/CharacterCard/CharacterCard.js
@@ -0,0 +1,74 @@
+
+// React Imports
+import React from 'react';
+
+// Material UI Imports
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+// import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+
+// Custom Styles
+import '../CharacterCard/CharacterCard.scss';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  button: {
+	padding: 0,
+	margin: 0
+  }
+});
+
+export default function CharacterCard({ character }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={`${classes.card} CardContainer`}>
+      <CardActionArea onClick={() => alert('This would give you a full snapshot of the characters information in their own profile view.')}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {character.name}
+          </Typography>
+			<Typography variant="body2" color="textSecondary" component="p">
+				Birth Year: {character.birth_year}
+          </Typography>
+			 <Typography variant="body2" color="textSecondary" component="p">
+				Gender: {character.gender}
+          </Typography>
+			 <Typography variant="body2" color="textSecondary" component="p">
+				Hair Color: {character.eye_color}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+				Hair Color: {character.skin_color}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+				Hair Color: {character.hair_color}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+				Height: {character.height}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className="Actions">
+        <Button size="small" variant="outlined" color="primary" onClick={() => alert('This would give you the full filmography of the character')}>
+          Filmography
+        </Button>
+        <Fab size="small" className={classes.button} color="secondary" onClick={() => alert('This would eventually save your characters in a new array.')}>
+          <Icon>favorite</Icon>
+        </Fab>
+      </CardActions>
+    </Card>
+  );
+}
